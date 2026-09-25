2758d6395aaa8538ce3b77b2da693a9ebfcda64e
panganibanmarkjohn159-ux <panganibanmarkjohn159@gmail.com>
2026-09-25 16:34:11 +0800

    Update MainActivity.kt

--- app/app/src/main/java/com/bia/sky_runner/java/com/bia/sky_runner/MainActivity.kt
+++ app/app/src/main/java/com/bia/sky_runner/java/com/bia/sky_runner/MainActivity.kt
@@ -1,1 +1,19 @@
+package com.bia.sky_runner
 
+import android.app.Activity
+import android.os.Bundle
+import android.view.WindowManager
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        setContentView(GameView(this))
+    }
+}
